98e53289b622772bbf9baf314fb818463808f99d
s338539679 <54991191+s338539679@users.noreply.github.com>
2020-01-26 19:25:16 -0500

    Completed AboutUs.js
    Screen about us with button back to main menu.

--- AboutUs.js
+++ AboutUs.js
@@ -1,13 +1,12 @@
-function setup() {
-  createCanvas(400, 400);
+function aboutUsSetup() {
 }
 
-function draw() {
+function aboutUsDraw() {
   background(168, 93, 137);
   
   //Title
   textSize (40);  
-  strokeWeight (90);
+  //strokeWeight (90);
   text("ABOUT US", 100, 75);
   fill (255);
   text("ABOUT US", 105, 75);
@@ -16,14 +15,14 @@ function draw() {
   
   textSize(20); 
   fill(0);
-  strokeWeight (20); 
+  //strokeWeight (5); 
   text ("This video game is our grade ten", 10, 150);
   text ("computer science summative.", 10, 170);
   
   // 2nd paragraph 
   text ("We've created this video game by using", 10, 210);
   text ("Javascript, HTML and CSS that we've learned", 10, 230);
-  text ("throughtout the course.", 10, 250); 
+  text ("throughout the course.", 10, 250); 
 
   
   text ("We hope you enjoy it!", 10, 300); 
@@ -32,10 +31,18 @@ function draw() {
   
   
   //back button 
- /* fill (255);
-  stroke(23);
-  rect(100,100,100,100);
- */ 
+  fill (255);
+  //stroke(23);
+  rect(10,10,60,30);
+  fill(0);
+  text("Back",14,34);
+  
 }
 
-
+function aboutUsMouseClicked() {
+ if(mouseX >= 10 && mouseX <= 70 &&
+      mouseY >= 10 && mouseY <= 40) {
+    // go to main menu
+    currentScene = 1; 
+ }
+}
